ba5fb2889f781b53b3bc9d35ece037b8d3ac4ab1
arpit1010 <mailmeonarpit@gmail.com>
2019-01-31 10:41:43 +0400

    Updates

---
+++ precache-manifest.84377366e7783b35cc41cfd233306182.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b0e5c5e4f591c137e55f",
+    "url": "/spacex-test/static/js/runtime~main.b0e5c5e4.js"
+  },
+  {
+    "revision": "1445dbab3a708c0f6b06",
+    "url": "/spacex-test/static/js/main.1445dbab.chunk.js"
+  },
+  {
+    "revision": "3bb6e7cdbdc63e7ceef2",
+    "url": "/spacex-test/static/js/1.3bb6e7cd.chunk.js"
+  },
+  {
+    "revision": "1445dbab3a708c0f6b06",
+    "url": "/spacex-test/static/css/main.ec953494.chunk.css"
+  },
+  {
+    "revision": "e3f8746f2a36e57fef764c5ae9f3f2ac",
+    "url": "/spacex-test/index.html"
+  }
+];
